47ddeee0cfb9820787ec3f0efa5fc95fb35f0ea2
Timo Matzen <timomatzen@re-stud-146-50-209-153.wireless.uva.nl>
2018-05-09 17:37:01 +0200

    - Added momentum - Added plots of the feature learning development

--- RBM_Function.R
+++ RBM_Function.R
@@ -4,21 +4,18 @@
 
 # Load in the train-data of the MNIST data-set
 train <- read.csv('train.csv', nrows = 200)
+
 # Put the data in a matrix of shap features * samples
 train <- matrix(unlist(train[,-1]), nrow =784,ncol = 200, byrow = T)/255
 
-image(matrix(train[,10], nrow = 28),col=grey.colors(255))
-n_hidden = 500
-
-
-
 ## Initialize RBM function
-RBM <- function(train, n_hidden, learning_rate = 0.1, n_iter, plot = FALSE){
+RBM <- function(train, n_hidden, learning_rate = 0.1, n_iter, 
+                plot = FALSE, mom = 0.9){
   
   # PLot the original data:
   if(plot == TRUE){
-    image(matrix(train[,10], nrow = 28),col=grey.colors(255))
-    title(main = "Original data", font.main = 4)
+    #image(matrix(train[,10], nrow = 28),col=grey.colors(255))
+    #title(main = "Original data", font.main = 4)
   }
   
   # Intialize the hidden layers (only one layer):
@@ -36,9 +33,23 @@ RBM <- function(train, n_hidden, learning_rate = 0.1, n_iter, plot = FALSE){
   vis_bias <- ifelse(vis_bias == -Inf, 0,vis_bias)
   # Initialize the weights, n_features * n_hidden:
   weights <- matrix(rnorm(nrow(train)*n_hidden, 0, .01), nrow = nrow(train), ncol = n_hidden)
+  # PLot the original data:
+  if(plot == TRUE){
+    #image(matrix(train[,10], nrow = 28),col=grey.colors(255))
+    #title(main = "Original data", font.main = 4)
+    par(mfrow = c(3,10), mar = c(3,1,1,1))
+    for(i in 1:n_hidden){
+      image(matrix(weights[,i], nrow = 28),col=grey.colors(255))
+      title(main = paste0('Hidden node ', i), font.main = 4)
+    }
+  }
   
+  # Initialize counter for the plotting:
   d <- 0
-  
+  # Initialize velocity at t = 0
+  vel_weights <- matrix(0, nrow = nrow(train), ncol = n_hidden)
+  vel_vis_bias <- matrix(0, nrow = nrow(train), ncol = 1)
+  vel_inv_bias <- matrix(0, nrow = n_hidden, ncol = 1)
   # Start contrastive divergence, k = 1:
   for (i in 1:n_iter){
     d <- d+1
@@ -64,20 +75,30 @@ RBM <- function(train, n_hidden, learning_rate = 0.1, n_iter, plot = FALSE){
     grad_vis_bias <- V0 - V1
     grad_inv_bias <- H0 - H1
     
+    ## Trying to use velocity instead of the gradient en learning_rate:
+    vel_weights <- (mom * vel_weights) + (grad_weights)
+    vel_vis_bias <- (mom * vel_vis_bias) + (grad_vis_bias)
+    vel_inv_bias <- (mom * vel_inv_bias) + (grad_inv_bias)
+  
     # Update bias and weights:
-    weights <- weights + (learning_rate * grad_weights)
-    vis_bias <- vis_bias + (learning_rate * grad_vis_bias)
-    inv_bias <- inv_bias + (learning_rate * grad_inv_bias)
+    weights <- weights + (learning_rate * vel_weights) # Use velocity instead (learning_rate * grad_weights)
+    vis_bias <- vis_bias + (learning_rate * vel_vis_bias) #(learning_rate * grad_vis_bias)
+    inv_bias <- inv_bias + (learning_rate * vel_inv_bias) #(learning_rate * grad_inv_bias)
     
     # Adding plots of learning if plot = TRUE:
-    if(i == 1 & plot == TRUE){
-      image(matrix(V1, nrow = 28),col=grey.colors(255))
-      title(main = paste0('Data at iteration ', i), font.main = 4)
-    }
-    if(d == 100 & plot == TRUE){
+    #if(i == 1 & plot == TRUE){
+     # image(matrix(V1, nrow = 28),col=grey.colors(255))
+      #title(main = paste0('Data at iteration ', i), font.main = 4)
+    #}
+    if(d == 1000 & plot == TRUE){
       # Plot figure at each 100 epoc
-      image(matrix(V1, nrow = 28),col=grey.colors(255))
-      title(main = paste0('Data at iteration ', i), font.main = 4)
+      #image(matrix(V1, nrow = 28),col=grey.colors(255))
+      #title(main = paste0('Data at iteration ', i), font.main = 4)
+      par(mfrow = c(3,10), mar = c(3,1,1,1))
+      for(i in 1:n_hidden){
+        image(matrix(weights[,i], nrow = 28),col=grey.colors(255))
+        title(main = paste0('Hidden node ', i), font.main = 4)
+      }
       d <- 0
       
     }
@@ -87,6 +108,14 @@ RBM <- function(train, n_hidden, learning_rate = 0.1, n_iter, plot = FALSE){
   return(weights)
 }
 
-weights <- RBM(train = train, n_hidden = 30, n_iter = 10000, learning_rate = .3)
+# Test the function:
+weights <- RBM(train = train, n_hidden = 30, n_iter = 10000, learning_rate = .1, plot = TRUE,
+               mom = 0.9)
 
+weights0 <-  matrix(rnorm(nrow(train)*n_hidden, 0, .01), nrow = nrow(train), ncol = n_hidden)
 
+par(mfrow = c(3,10), mar = c(3,1,1,1))
+for(i in 1:n_hidden){
+  image(matrix(weights[,i], nrow = 28),col=grey.colors(255))
+  title(main = paste0('Hidden node ', i), font.main = 4)
+}
