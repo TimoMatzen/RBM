4f2109817a8bb0084167d90a38b374f71aa68933
Timo Matzen <timomatzen@vpn-stud-146-50-146-30.vpn.uva.nl>
2018-05-28 16:58:25 +0200

    - Improved DBN with batch gradient descent. Accuracy 86% !

--- DBNHinton.R
+++ DBNHinton.R
@@ -32,8 +32,8 @@ DBN <- function(x, y, n.iter.pre = 30, n.iter = 300, nodes = c(30,40,30),
   
   # Start the wake sleep algorithm
   for (j in 1:n.iter) {
-    # Take a sample of each class balanced
-    samp <- sample(1:nrow(x), size.minibatch)
+    # Pick balanced labels
+    samp <- sample(1:nrow(x),size.minibatch)
     
     # First perform one feed firward pass
     # Sample vis from the data
@@ -58,10 +58,12 @@ DBN <- function(x, y, n.iter.pre = 30, n.iter = 300, nodes = c(30,40,30),
     # Calculate the cost
     J <- 1/size.minibatch* (sum(-t(y[samp,,drop = FALSE]) * log(lab) - ((1 - t(y[samp,,drop = FALSE])) * log(1 - lab))))
     J <- sum(J)
-    # Stopping criterion
-    if (J < 1) break
+    
     # Print cost
     print(paste0('Cost at epoch ', j, ' = ', J))
+    
+    # Stopping criterion
+    #if (J < 1) break
     
     # Compare label to actual label and backpropogate
     er.grad.lab <- logistic(lab) * (1 - logistic(lab)) * -(t(y[samp,,drop = FALSE]) -  lab)
@@ -84,22 +86,22 @@ DBN <- function(x, y, n.iter.pre = 30, n.iter = 300, nodes = c(30,40,30),
   
     # Adjust weights
     # hidden layer
-    grad.hid <- learning.rate  * (  er.grad.hid %*% vis)
+    grad.hid <- learning.rate  * 1/size.minibatch * (er.grad.hid %*% vis)
     # adjust weights hidden
-    weights[[1]]$trained.weights <- weights[[1]]$trained.weights - grad.hid
+    weights[[1]]$trained.weights <- weights[[1]]$trained.weights - grad.hid  
     
     # Pentative layer
-    grad.pen <- learning.rate * ( er.grad.pen %*% t(hid))
+    grad.pen <- learning.rate * 1/size.minibatch * ( er.grad.pen %*% t(hid))
     # Adjust weights pentative layer
     weights[[2]]$trained.weights <- weights[[2]]$trained.weights - grad.pen
     
     # Top layer
-    grad.top <- learning.rate * (er.grad.top %*% t(pen))
+    grad.top <- learning.rate * 1/size.minibatch * (er.grad.top %*% t(pen))
     # Adjust weights top layer
     weights[[3]]$trained.weights <- weights[[3]]$trained.weights - grad.top
     
     # Label weights
-    grad.lab <- learning.rate * (er.grad.lab %*% t(top))
+    grad.lab <- learning.rate * 1/size.minibatch * (er.grad.lab %*% t(top))
     y.weights <- y.weights - grad.lab
     
     
@@ -110,12 +112,9 @@ DBN <- function(x, y, n.iter.pre = 30, n.iter = 300, nodes = c(30,40,30),
   return(weights)
 }
 
-# Take a sample
-samp <- sample(1 : nrow(test),1)
-# Sample label 
-y <- test_y[samp]
+
 # Sample visible
-vis <- cbind(1, test[samp,,drop = FALSE])
+vis <- cbind(1, test)
 
 H <- logistic(mod[[1]]$trained.weights %*% t(vis))
 H <- rbind(1, H)
@@ -128,5 +127,6 @@ H <- logistic(mod[[3]]$trained.weights %*% H)
 H <- rbind(1, H)
 
 lab <- logistic(mod[[4]] %*% H)
-lab
-y
+
+# Calculate accuracy
+mean(apply(lab, 2, which.max)-1 == test_y)
