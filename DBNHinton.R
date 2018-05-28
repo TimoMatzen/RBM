e06ff836bde35ba30414891c90bd168ce68f7093
Timo Matzen <timomatzen@vpn-stud-146-50-146-30.vpn.uva.nl>
2018-05-28 15:51:17 +0200

    - Working version DBN with pretraining and backprop! :D

--- DBNHinton.R
+++ DBNHinton.R
@@ -1,128 +1,132 @@
-DBNHinton <- function(x, y, n.iter.pre = 100, n.iter = 440, n.CD.iters = 1 , nodes = c(30,40,30), learning.rate = 0.1, size.mini.batch = 10) {
-  # Initialize the weights
-  # First create list for weights, size 3
-  # Pretrain the system
-  weights <- pretrain(x,y,n.iter= n.iter.pre, layers = nodes, learning.rate = learning.rate, size.minibatch = size.mini.batch )
+DBN <- function(x, y, n.iter.pre = 30, n.iter = 300, nodes = c(30,40,30),
+                      learning.rate = 0.5, size.minibatch = 1, learning.rate.pre = .1) {
+
+ 
+  # Initialize weights with the pretrain algorithm
+  weights <- PretrainGreedy(x,  n.iter= n.iter.pre, layers = nodes, 
+                            learning.rate = learning.rate.pre, size.minibatch = size.minibatch )
+  
+  # Remove bias weights in opposite directions
+  weights[[1]]$trained.weights <- t(weights[[1]]$trained.weights)[-1,]
+  weights[[2]]$trained.weights <- t(weights[[2]]$trained.weights)[-1,]
+  weights[[3]]$trained.weights <- t(weights[[3]]$trained.weights)[-1,]
+  
+  # Get all the indexes for the unique labels
   labels <- unique(y)
   idx <- vector('list', length = length(labels))
+  
+  # Initialize the y weights
+  y.weights <- matrix(rnorm(length(labels) * nodes[3], 0, 01), 
+                                    nrow = length(labels) , ncol = nodes[3])
+  # Add term for the bias
+  y.weights <- cbind(rnorm(length(labels)), y.weights)
   # Save indexes
   for (i in 1:length(labels)) {
     idx[[i]]<- which(y == labels[i])
   }
   y <- LabelBinarizer(y)
   
-  # Initialize all the weights from a gaussian
-  
-  #for (n in 1:length(nodes)) {
-    # Initialize the weights, features * hidden
-   # if (n == 1){
-    #  weights[[n]]$weights <- matrix(rnorm(nrow(x)*nodes[n], 0, .01), nrow = nrow(x), ncol = nodes[n])
-      
-    #} else if (n == length(nodes)) {
-     # weights[[n]]$weights <- matrix(rnorm(nodes[n-1]*nodes[n], 0, .01), nrow = nodes[n-1], ncol = nodes[n])
-      #weights[[n]]$y.weights <- matrix(rnorm(ncol(y)*nodes[n], 0, .01), nrow = ncol(y), ncol = nodes[n])
-      #weights[[n]]$y.weights <- cbind(0, weights[[n]]$y.weights)
-      #weights[[n]]$y.weights <- rbind(0, weights[[n]]$y.weights)
-    #} else {
-      #weights[[n]]$weights <- matrix(rnorm(nodes[n-1]*nodes[n], 0, .01), nrow = nodes[n-1], ncol = nodes[n])
-    #}
-    
-    ## Add term for the bias
-    #weights[[n]]$weights <- cbind(0, weights[[n]]$weights)
-    #weights[[n]]$weights <- rbind(0, weights[[n]]$weights)
-  #}
   # Attach bias to data
-  x <- rbind(1, x)
-  # Add one term for the bias
-  y <- cbind(1, y)
+  x <- cbind(1, x)
   
+  
+  # Start the wake sleep algorithm
   for (j in 1:n.iter) {
     # Take a sample of each class balanced
-    samp <- rep(0,size.minibatch)
-    p <- 1
-    for (i in 1 : size.minibatch){
-      samp[p]<- sample(idx[[p]], 1)
-      p <- p + 1
-      if (p == length(labels)+1) {
-        # Reset counter
-        p <- 1
-      }
-    }
-    # First perform one bottom up pass to get weights
-    # Sample vis 
-    vis <- x[, samp, drop = FALSE]
-    # Calculate first hidden 
-    wake.hid.probs <- VisToHid(vis, weights[[1]]$trained.weights)
-    wake.hid.states <- wake.hid.probs > runif(length(wake.hid.probs))
+    samp <- sample(1:nrow(x), size.minibatch)
+    
+    # First perform one feed firward pass
+    # Sample vis from the data
+    vis <- x[ samp, ,drop = FALSE]
+    # Go to first hidden layer
+    hid <- logistic(weights[[1]]$trained.weights %*% t(vis))
     # Fix bias
-    wake.hid.states[1, ] <- 1
+    hid <- rbind(1, hid)
+    # Go to second hidden layer, first hidden layer as input
+    pen <- logistic(weights[[2]]$trained.weights %*% hid)
+    # Fix the bias
+    pen <- rbind(1, pen)
+    
+    # Go to third hidden layer, second hidden layer as input
+    top <- logistic(weights[[3]]$trained.weights %*% pen)
+    # Fix the bias
+    top <- rbind(1, top)
+
+    # Now the last hidden to label output
+    lab <- logistic(y.weights %*% top)
+    
+    # Calculate the cost
+    J <- 1/size.minibatch* (sum(-t(y[samp,,drop = FALSE]) * log(lab) - ((1 - t(y[samp,,drop = FALSE])) * log(1 - lab))))
+    J <- sum(J)
+    # Stopping criterion
+    if (J < 1) break
+    # Print cost
+    print(paste0('Cost at epoch ', j, ' = ', J))
+    
+    # Compare label to actual label and backpropogate
+    er.grad.lab <- logistic(lab) * (1 - logistic(lab)) * -(t(y[samp,,drop = FALSE]) -  lab)
+    
+    # Backpropogate the error
+    top.back <- t(y.weights[,-1]) %*% er.grad.lab
+    
+    # Calculate gradient top layer
+    er.grad.top <- logistic(top[-1,]) * (1 - logistic(top[-1,])) * top.back
+    
+    # Back to pentative layer
+    pen.back <- t(weights[[3]]$trained.weights[,-1]) %*% er.grad.top
+    # calculate gradient pentative layer
+    er.grad.pen <- logistic(pen[-1, ]) * (1 - logistic(pen[-1,])) * pen.back
+   
+    # Back to hidden layer 
+    hid.back <- t(weights[[2]]$trained.weights[,-1]) %*% er.grad.pen
+    # Calculate gradient hidden layer
+    er.grad.hid <- logistic(hid[-1,]) * (1 - logistic(hid[-1,])) * hid.back
   
-    # Calculate second hidden 
-    wake.pen.probs <- VisToHid(wake.hid.states, weights[[2]]$trained.weights)
-    wake.pen.states <- wake.pen.probs > runif(length(wake.pen.probs))
-    # Fix bias
-    wake.pen.states[1, ] <- 1
+    # Adjust weights
+    # hidden layer
+    grad.hid <- learning.rate  * (  er.grad.hid %*% vis)
+    # adjust weights hidden
+    weights[[1]]$trained.weights <- weights[[1]]$trained.weights - grad.hid
     
-    # Calculate last hidden
-    wake.top.probs <- VisToHid(wake.pen.states, weights[[3]]$trained.weights, 
-                              y[samp,,drop = FALSE], weights[[3]]$trained.y.weights)
-    wake.top.states <- wake.top.probs > runif(length(wake.top.probs))
+    # Pentative layer
+    grad.pen <- learning.rate * ( er.grad.pen %*% t(hid))
+    # Adjust weights pentative layer
+    weights[[2]]$trained.weights <- weights[[2]]$trained.weights - grad.pen
     
-    wake.top.states[1,] <- 1
-    # Positive phase 
-    pos.lab.top.statistics <- wake.top.states %*% y[samp,,drop = FALSE]
-    pos.pen.top.statistics <- wake.top.states %*% t(wake.pen.states )
+    # Top layer
+    grad.top <- learning.rate * (er.grad.top %*% t(pen))
+    # Adjust weights top layer
+    weights[[3]]$trained.weights <- weights[[3]]$trained.weights - grad.top
     
-    neg.top.states <- wake.top.states
+    # Label weights
+    grad.lab <- learning.rate * (er.grad.lab %*% t(top))
+    y.weights <- y.weights - grad.lab
     
-    for (i in 1:n.CD.iters) {
-      neg.pen.probs <- HidToVis(neg.top.states, weights[[3]]$trained.weights) 
-      neg.pen.states <- neg.pen.probs > runif( nodes[2]+1)
-      neg.pen.states[1, ] <- 1
-      neg.lab.probs <- HidToVis(neg.top.states, weights[[3]]$trained.weights, weights[[3]]$trained.y.weights)
-      neg.lab.probs[ 1,] <- 1
-      neg.top.probs <-  VisToHid(neg.pen.states, weights[[3]]$trained.weights, 
-                                 t(neg.lab.probs), weights[[3]]$trained.y.weights)
-      neg.top.probs[1, ] <- 1
-      neg.top.states <- neg.top.probs > runif(length(neg.top.probs))
-      neg.top.states[1,] <- 1
-    }
     
-    # NEGATIVE PHASE STATISTICS FOR CONTRASTIVE DIVERGENCE
-    neg.pen.top.statistics <- neg.pen.states %*% t(neg.top.states)
-    neg.lab.top.statistics <- neg.lab.probs %*% t(neg.top.states)
     
-    # STARTING FROM THE END OF THE GIBBS SAMPLING RUN, PERFORM A
-    # TOP-DOWN GENERATIVE PASS TO GET SLEEP/NEGATIVE PHASE PROBABILITIES
-    # AND SAMPLE STATES
-    sleep.pen.states <- neg.pen.states
-    sleep.hid.probs <- HidToVis(sleep.pen.states, weights[[2]]$trained.weights)
-    sleep.hid.states <- sleep.hid.probs > runif(nodes[1]+1)
-    sleep.hid.states[1,] <- 1
-    sleep.vis.probs <- HidToVis(sleep.hid.states, weights[[1]]$trained.weights )
-    sleep.vis.probs[1, ] <- 1
-    
-    # PREDICTIONS
-    psleep.pen.states <- VisToHid(sleep.hid.states, weights[[2]]$trained.weights)
-    psleep.pen.states[1, ] <- 1 
-    psleep.hid.states <- VisToHid(sleep.vis.probs, weights[[1]]$trained.weights )
-    psleep.hid.states[1, ] <- 1
-    pvis.probs <- VisToHid(wake.hid.states, t(weights[[1]]$trained.weights))
-    pvis.probs[1,] <- 1
-    phid.probs <- VisToHid(wake.pen.states, t(weights[[2]]$trained.weights))
-    phid.probs[1, ] <- 1
-    
-    # UPDATES TO GENERATIVE PARAMETERS
-    weights[[1]]$trained.weights <- weights[[1]]$trained.weights + (learning.rate * (vis-pvis.probs) %*% t(wake.hid.states))
-    weights[[2]]$trained.weights <- weights[[2]]$trained.weights + (learning.rate * (wake.hid.states-phid.probs) %*% t(wake.pen.states))
-    
-    # UPDATES TO TOP LEVEL ASSOCIATIVE MEMORY PARAMETERS
-    weights[[3]]$trained.y.weights <- weights[[3]]$trained.y.weights + (learning.rate *(t(pos.lab.top.statistics)-neg.lab.top.statistics))
-    weights[[3]]$trained.weights <- weights[[3]]$trained.weights + (learning.rate* (t(pos.pen.top.statistics) - neg.pen.top.statistics))
-    
-    # UPDATES TO RECOGNITION/INFERENCE APPROXIMATION PARAMETERS
-    weights[[2]]$trained.weights <- weights[[2]]$trained.weights + ( learning.rate * sleep.hid.states %*% t(sleep.pen.states-psleep.pen.states))
-    weights[[1]]$trained.weights <- weights[[1]]$trained.weights + (learning.rate *  sleep.vis.probs %*% t(sleep.hid.states-psleep.hid.states))
   }
+  # Add the label weights to the weights
+  weights[[4]] <- y.weights
   return(weights)
 }
+
+# Take a sample
+samp <- sample(1 : nrow(test),1)
+# Sample label 
+y <- test_y[samp]
+# Sample visible
+vis <- cbind(1, test[samp,,drop = FALSE])
+
+H <- logistic(mod[[1]]$trained.weights %*% t(vis))
+H <- rbind(1, H)
+
+
+H <- logistic(mod[[2]]$trained.weights %*% H)
+H <- rbind(1, H)
+
+H <- logistic(mod[[3]]$trained.weights %*% H)
+H <- rbind(1, H)
+
+lab <- logistic(mod[[4]] %*% H)
+lab
+y
